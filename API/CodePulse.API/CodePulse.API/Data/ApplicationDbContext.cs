0f6415951ef860b9b8fac0580c1144961d9f510f
Jaydeep Gawade <jaydeepgawade42@gmail.com>
2025-05-22 23:15:06 +0530

    Initial commit from Udemy project

---
+++ API/CodePulse.API/CodePulse.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CodePulse.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        #region Ctor
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        #endregion
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ API/CodePulse.API/CodePulse.API/Models/Domain/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace CodePulse.API.Models.Domain
+{
+    public class Category
+    {
+        #region Properties
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+
+        #endregion
+    }
+}
